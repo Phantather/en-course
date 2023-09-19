4cca4d98ca46db4923c724ea1b22aa775112420a
Phantather <akdil.hstech@gmail.com>
2023-09-19 23:20:32 +0600

    change all project

---
+++ js/index.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const accordionHeaders = document.querySelectorAll(".accordion-header");
+
+    accordionHeaders.forEach(function (header) {
+        header.addEventListener("click", function () {
+            const content = this.nextElementSibling;
+            if (content.style.display === 'block') {
+                content.style.display = 'none';
+                this.classList.remove("open");
+            } else {
+                closeAllAccordionItems();
+                content.style.display = 'block';
+                this.classList.add("open");
+            }
+        });
+    });
+
+    function closeAllAccordionItems() {
+        const accordionContents = document.querySelectorAll(".accordion-content");
+        const accordionHeaders = document.querySelectorAll(".accordion-header");
+        accordionContents.forEach(function (content) {
+            content.style.display = 'none';
+        });
+        accordionHeaders.forEach(function (header) {
+            header.classList.remove("open");
+        });
+    }
+});
